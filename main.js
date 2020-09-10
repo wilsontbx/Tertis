73cc927563a7f595a3308132356e580d7eccdb41
wilsontbx <boonxian1993@hotmail.com>
2020-09-10 22:53:35 +0800

    Print block at board

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 const board = document.getElementById('tertisBoard')
 const context = board.getContext('2d')
-context.scale(30, 30)
+// context.scale(30, 30)
 
 context.fillStyle = 'white'
-context.fillRect(0, 0, 10, 24); 
+context.fillRect(0, 0, 300, 720); 
+
+let blockT = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [1, 1, 1]
+]
+
+const player = {                                
+    matrix: blockT,
+    position: { x: 0, y: 0 },
+}
+
+function generateBlock(block, offset) {         // this function is generate block in screen offset is to move the block
+    block.forEach((row, y) => {
+        row.forEach((val, x) => {
+            if (val === 1) {
+                context.fillStyle = "red"
+                context.fillRect((x + offset.x)*30, (y + offset.y)*30, 30, 30)
+                context.strokeRect((x + offset.x)*30, (y + offset.y)*30, 30, 30)
+            }
+        })
+    })
+}
+
+generateBlock(player.matrix, player.position)
